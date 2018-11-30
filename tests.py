14375d38ba23173911cb1652a24951326cae8dd7
khwilo <khwilowatai@gmail.com>
2018-11-30 11:07:23 +0300

    [Starts #162336786] Add project wide test file

---
+++ tests.py
@@ -0,0 +1,13 @@
+from app import create_app
+
+import unittest
+import os 
+import json
+
+class IncidenceTestCase(unittest.TestCase):
+    
+    def test_incidence_creation(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
