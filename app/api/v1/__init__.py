b3c27be7437f659f9e6427d6fa635eb570148ca1
khwilo <khwilowatai@gmail.com>
2018-12-03 17:44:29 +0300

    [Starts #162377808] Create the definition of the authentication blueprint and add it as a resource

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,13 +3,15 @@ from flask_restful import Api
 from app.api.v1.views import *
 
 api_blueprint = Blueprint("api", __name__, url_prefix='/api/v1')
+auth_blueprint = Blueprint("auth", __name__, url_prefix='/auth')
 
 api = Api(api_blueprint)
+auth_api = Api(auth_blueprint)
 
 api.add_resource(RedFlagList, '/red-flags')
 api.add_resource(RedFlag, '/red-flags/<id>')
 api.add_resource(RedFlagLocation, '/red-flags/<id>/location')
 api.add_resource(RedFlagComment, '/red-flags/<id>/comment')
 
-api.add_resource(UserRegistration, '/register')
-api.add_resource(UserLogin, '/login')
+auth_api.add_resource(UserRegistration, '/register')
+auth_api.add_resource(UserLogin, '/login')
